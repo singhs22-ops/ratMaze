82809e5f8cb5a18e09c772a602b121c78bdd3988
Himanshu Singh <himanshusingh038@gmail.com>
2022-10-08 10:54:45 +0530

    yml

---
+++ src/main/java/com/example/ratMazeproblem/ratMazeproblem/config/RatMazeproblemApplication.java
@@ -0,0 +1,13 @@
+package com.example.ratMazeproblem.ratMazeproblem.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatMazeproblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatMazeproblemApplication.class, args);
+	}
+
+}
