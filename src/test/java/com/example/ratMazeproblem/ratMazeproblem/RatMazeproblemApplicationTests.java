2110f44eae07a48cf1f52f1f2a5366b163557d35
Himanshu Singh <himanshusingh038@gmail.com>
2022-10-08 10:28:42 +0530

    Initial commit

---
+++ src/test/java/com/example/ratMazeproblem/ratMazeproblem/RatMazeproblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ratMazeproblem.ratMazeproblem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatMazeproblemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
